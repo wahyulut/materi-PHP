3250d127a4fba2b79272d14dba93a12424103d6f
Lutvi Wahyudi <87699531+wahyulut@users.noreply.github.com>
2022-06-08 14:44:56 +0700

    Ini sintax dasar pada PHP

---
+++ PHP syntax penulisan.php
@@ -0,0 +1,28 @@
+<?php 
+// Pertemuan 2 - PHP dasar
+// sintaks php
+
+//standar output
+//echo, print
+//print_r
+//var_dump
+
+//penulisan sintaks php 
+//1. php didalam html
+
+//variable dan tipe data
+
+//operator
+//aritmatika
+$x = 10;
+$y = 20;
+echo $x + $y;
+
+//aritmatika
+echo $x * $y;
+//aritmatika
+echo $x - $y;
+//aritmatika 
+echo $x / $y;
+
+?>
